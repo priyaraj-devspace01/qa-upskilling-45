32ea77d77d2568b68565fe473bcc46c900ada0b5
Priyaraj Mohanty <priyarajmohanty@Priyaraj-Mohanty-MacBook-Pro-M2.local>
2026-05-25 22:11:24 +0530

    late code addtion for last part of selenium day-09 code added

---
+++ src/main/com/test/skup/advancedWebActionHandling/handleCalenderTable/DemoDatePickerHandsON.java
@@ -0,0 +1,34 @@
+package src.main.com.test.skup.advancedWebActionHandling.handleCalenderTable;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DemoDatePickerHandsON {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hyrtutorials.com/p/calendar-practice.html");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+       driver.findElement(By.xpath("//table[@id='datepickers']//tr//td//input[@id='first_date_picker']")).click();
+       // click on the calendar
+        while(!driver.findElement(By.xpath("//div[@class='ui-datepicker-title']")).getText().contains("December"))
+        {
+            driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).click();
+            Thread.sleep(500);
+        }
+
+        System.out.println("December got selected . ");
+
+
+        Thread.sleep(3000);
+        driver.quit();
+
+
+
+    }
+}
---
+++ src/main/com/test/skup/advancedWebActionHandling/handleCalenderTable/HandleTablesHandsON.java
@@ -0,0 +1,48 @@
+package src.main.com.test.skup.advancedWebActionHandling.handleCalenderTable;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class HandleTablesHandsON
+{
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/webtables");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        // let's suppose we want to print all the table headers
+        List<WebElement> headers = driver.findElements(By.xpath("//table[@class=\"-striped -highlight table table-striped table-bordered table-hover\"]//thead//tr//th"));
+        System.out.println(headers.size());
+        for(WebElement header  :headers)
+        {
+            System.out.print(header.getText()+  " | ");
+
+        }
+        System.out.println();
+
+       // NOW IF WE ALREADY KNOW THE ROW NUMBER AND COLUMN NUMBER WE CAN EASILY GET THE DATA
+        // LET
+         int row = 2;
+        int column = 3;
+
+        String xpath = "//table[contains(@class,'table-striped')]//tbody//tr["
+                + row + "]//td[" + column + "]";
+
+        WebElement value = driver.findElement(By.xpath(xpath));
+
+        System.out.println("the value at required index is : " + value.getText());
+        System.out.println();
+        Thread.sleep(2000);
+
+        driver.quit();
+
+    }
+}
